bc2b0d197ac95aef824a0fd04771e96408fd6697
Nguyen Nhat Nguyen <nhatnguyen741@gmail.com>
2020-06-16 21:41:21 +0700

    commit #002
    - Implicit join syntax: we can write the join syntax shortly using implicit.
    -> But you should using when you know exact about join

---
+++ implicit-join-syntax/implicit-join-syntax.sql
@@ -0,0 +1,9 @@
+-- SELECT * 
+-- FROM orders o
+-- JOIN customers c
+-- 	ON o.customer_id = c.customer_id
+    
+-- Implicit join syntax
+SELECT *
+FROM orders o, customers c 
+WHERE o.customer_id = c.customer_id
